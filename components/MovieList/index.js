9edbf0935f0c214fe4f80daa9baec203c5f3c8ab
Sascha Pionczewski <pionczewski.sascha@gmail.com>
2023-03-23 15:55:25 +0100

    changed from mongo compass to atlas

--- components/MovieList/index.js
+++ components/MovieList/index.js
@@ -5,38 +5,6 @@ import Link from "next/link";
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
-// export default function MovieList() {
-//   const { data: movies, error } = useSWR(
-//     "https://mcuapi.herokuapp.com/api/v1/movies",
-//     fetcher
-//   );
-//   if (error) {
-//     return <div>Failed to load from API</div>;
-//   }
-//   if (!movies) {
-//     return <div>Loading...</div>;
-//   }
-
-//   return (
-//     <div>
-//       {movies.data
-//         .map((movie) => (
-//           <Link href={`/${movie.id}`} key={movie.id}>
-//             <h2>{movie.title}</h2>
-//             <p>{movie.release_date}</p>
-//             <Image
-//               src={movie.cover_url}
-//               height={300}
-//               width={200}
-//               alt={movie.title}
-//             />
-//           </Link>
-//         ))
-//         .reverse()}
-//     </div>
-//   );
-// }
-
 export default function MovieList() {
   const { data: movies, error } = useSWR("/api/movies", fetcher);
   if (error) {
