635101deb65f0099cd49b7f53a1eea00766042f6
Sascha Pionczewski <pionczewski.sascha@gmail.com>
2023-04-08 13:35:23 +0200

    add the dark mode

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,43 @@
-import GlobalStyle from "../styles";
+import { GlobalStyles, lightTheme, darkTheme } from "../contexts/ThemeConfig";
 import Head from "next/head";
 import NavBar from "../components/NavBar";
 import { BookmarkProvider } from "../contexts/BookmarkContext";
 import "../public/fonts.css";
+import { ThemeProvider } from "styled-components";
+import { useEffect, useState, createContext } from "react";
+
+export const ThemeContext = createContext();
 
 export default function App({ Component, pageProps }) {
+  const [theme, setTheme] = useState(() => {
+    if (typeof window !== "undefined") {
+      return localStorage.getItem("ThemeMode");
+    }
+    return "light";
+  });
+
+  const toggleTheme = () => {
+    theme === "light" ? setTheme("dark") : setTheme("light");
+  };
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      localStorage.setItem("ThemeMode", theme);
+    }
+  }, [theme]);
+
   return (
     <>
-      <GlobalStyle />
       <Head>
         <title>Marvel Movie App</title>
       </Head>
       <BookmarkProvider>
-        <Component {...pageProps} />
+        <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+          <ThemeContext.Provider value={{ toggleTheme }}>
+            <GlobalStyles />
+            <Component {...pageProps} />
+          </ThemeContext.Provider>
+        </ThemeProvider>
       </BookmarkProvider>
       <NavBar />
     </>
--- pages/settings.js
+++ pages/settings.js
@@ -1,12 +1,57 @@
-import React from "react";
-import Image from "next/image";
+import { useContext } from "react";
 import Heading from "../components/Heading";
+import { ThemeContext } from "./_app";
+import styled from "styled-components";
 
 export default function Settings() {
+  const { toggleTheme } = useContext(ThemeContext);
+
   return (
     <>
       <Heading>Settings</Heading>
-      <Image src="/pngwing.com (1).png" alt="groot" width={300} height={500} />
+      <h2>Dark Mode:</h2>
+      <div>
+        <StyledInput type="checkbox" id="toggle" onClick={toggleTheme} />
+        <StyledLabel htmlFor="toggle"></StyledLabel>
+      </div>
     </>
   );
 }
+
+const StyledInput = styled.input`
+  visibility: hidden;
+  &:checked + label {
+    transform: rotate(360deg);
+    background-color: #000;
+    &:before {
+      transform: translateX(50px);
+      background-color: #fff;
+    }
+  }
+`;
+
+const StyledLabel = styled.label`
+  display: flex;
+  width: 90px;
+  height: 45px;
+  border: 4px solid;
+  border-radius: 99em;
+  position: relative;
+  transition: transform 0.75s ease-in-out;
+  transform-origin: 50% 50%;
+  cursor: pointer;
+
+  &:before {
+    transition: transform 0.75s ease;
+    transition-delay: 0.5s;
+    content: "";
+    display: block;
+    position: absolute;
+    width: 27px;
+    height: 27px;
+    background-color: #000;
+    border-radius: 50%;
+    top: 9px;
+    left: 6px;
+  }
+`;
