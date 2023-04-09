7efb701572cb4985f073106870918c00df4be4e1
Sascha Pionczewski <pionczewski.sascha@gmail.com>
2023-04-09 10:44:47 +0200

    finished the dark mode

--- pages/settings.js
+++ pages/settings.js
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import Heading from "../components/Heading";
 import { ThemeContext } from "./_app";
 import styled from "styled-components";
+import Backbutton from "../components/Backbutton";
 
 export default function Settings() {
   const { toggleTheme } = useContext(ThemeContext);
@@ -9,11 +10,12 @@ export default function Settings() {
   return (
     <>
       <Heading>Settings</Heading>
-      <h2>Dark Mode:</h2>
-      <div>
+      <StyledSettingsContainer>
+        <Backbutton />
+        <h2>Dark Mode:</h2>
         <StyledInput type="checkbox" id="toggle" onClick={toggleTheme} />
         <StyledLabel htmlFor="toggle"></StyledLabel>
-      </div>
+      </StyledSettingsContainer>
     </>
   );
 }
@@ -45,7 +47,7 @@ const StyledLabel = styled.label`
     transition: transform 0.75s ease;
     transition-delay: 0.5s;
     content: "";
-    display: block;
+    // display: block;
     position: absolute;
     width: 27px;
     height: 27px;
@@ -54,4 +56,11 @@ const StyledLabel = styled.label`
     top: 9px;
     left: 6px;
   }
+`;
+
+const StyledSettingsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
 `;
